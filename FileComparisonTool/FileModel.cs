5944cc4f62a7c3262894a851b084e555b0e25b96
村长 <757891685@qq.com>
2019-11-13 10:21:08 +0800

    项目提交

---
+++ FileComparisonTool/FileModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileComparisonTool
+{
+    public class FileModel
+    {
+        /// <summary>
+        /// 目录
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 文件
+        /// </summary>
+        public List<string> Files { get; set; }
+
+        /// <summary>
+        /// 子目录
+        /// </summary>
+        public List<FileModel> Childs { get; set; } = new List<FileModel>();
+    }
+}
